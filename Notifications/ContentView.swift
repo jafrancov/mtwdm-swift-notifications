54dfc10e26e6efd73ec8c44c3250cb3780894d33
Alejandro Franco <jafrancov@gmail.com>
2020-03-12 23:56:40 -0600

    Initial Commit

---
+++ Notifications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notifications
+//
+//  Created by Alejandro Franco on 12/03/20.
+//  Copyright © 2020 Alejandro Franco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
